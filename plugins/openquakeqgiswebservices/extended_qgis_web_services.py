00e44f86c4a23755ca7af5f2cc53373d8b3f362e
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2019-03-18 11:14:40 +0100

    Fix filtering

--- plugins/openquakeqgiswebservices/extended_qgis_web_services.py
+++ plugins/openquakeqgiswebservices/extended_qgis_web_services.py
@@ -43,17 +43,14 @@ class EWMS(QgsService):
             layer_names = layer_names_str.split(',')
         else:
             layer_names = None
-        print('layer_names: %s' % layer_names)
         if filter_str:
             filter_prop_items = [filter_prop.split(':')
                                  for filter_prop in filter_str.split(',')]
-            print('filter_prop_items: %s' % filter_prop_items)
             filter_props = {
                 filter_prop_name: filter_prop_value
                 for filter_prop_name, filter_prop_value in filter_prop_items}
         else:
             filter_props = None
-        print('filter_props: %s' % filter_props)
         custom_props = {}
         for layer_id, layer in project.mapLayers().items():
             layer_name = layer.name()
@@ -64,18 +61,15 @@ class EWMS(QgsService):
             for prop in layer.customPropertyKeys():
                 prop_value = layer.customProperty(prop)
                 custom_props[layer_name][prop] = prop_value
-        print('custom_props: %s' % custom_props)
+        custom_props_filtered = custom_props.copy()
         for filter_prop in filter_props:
             for layer in custom_props:
-                print('custom_props[%s][%s]: %s'
-                      % (layer, filter_prop, custom_props[layer][filter_prop]))
                 filter_prop_value = filter_props[filter_prop]
                 if custom_props[layer][filter_prop] != filter_prop_value:
-                    del custom_props[layer]
-                    break
+                    del custom_props_filtered[layer]
         response.setStatusCode(200)
         response.write(
-            json.dumps(custom_props, indent=4, sort_keys=True))
+            json.dumps(custom_props_filtered, indent=4, sort_keys=True))
 
     def _get_custom_properties_by_layer_id(self, request, response, project):
         custom_props = {}
